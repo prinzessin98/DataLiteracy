9b141d012a2b2efe629c0f6ba9b61d53d1cdcd98
prinzessin98 <121862076+prinzessin98@users.noreply.github.com>
2023-01-03 11:34:41 +0100

    Add files via upload

---
+++ data_lit_project.py
@@ -0,0 +1,23 @@
+#import math
+#import scipy
+import numpy as np
+#import matplotlib.pyplot as plot
+import pandas as pd 
+
+##Loading the data
+path ="C:/Users/Fynn/Documents/Uni/Master/DataLiteracy/Project/Daten/data.csv"
+raw_df =  pd.read_csv(path, header=1,sep=";", names=['date','Tunnel','Steinlach','Hirschau'])
+#sum up cyclists over each day
+raw_df.head
+raw_df['date']=pd.to_datetime(raw_df['date']).dt.date
+raw_df=raw_df.groupby(['date']).sum()
+df=raw_df.copy()
+print(df.head())
+#introduce weeks as new time index to sum up days
+weeks = pd.date_range('2018-01-01', '2022-11-27', freq='W').to_numpy()
+weeks= np.repeat(weeks, 7)
+#reshape the dataframe to show the sum of all cyclists of a week
+df['week']= weeks.tolist()
+df['week']=pd.to_datetime(df['week']).dt.date
+df=df.groupby('week').sum()
+print(df.head())
