21cab94bb6c2b987eb699d011181a3f2ad45b82c
Fynn <fynnlu@gmail.com>
2023-01-08 14:23:26 +0100

    plot weeks separeted for train dates

--- data_lit_project.py
+++ data_lit_project.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd 
+from sklearn.linear_model import LinearRegression as LinReg
 from pathlib import Path
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
@@ -24,8 +25,31 @@ weeks= np.repeat(weeks, 7)
 df['week']= weeks.tolist()
 df['week']=pd.to_datetime(df['week']).dt.date
 df=df.groupby('week').sum()
-print(df.head())
+print(df.tail())
 #plotten
 #plt.plot(raw_df)
-plt.plot(df)
+#plt.plot(df)
+#plt.show()
+dates18=pd.date_range('2018-01-01', '2018-04-05', freq='W')
+dates19=pd.date_range('2019-01-01', '2019-04-05', freq='W')
+dates20=pd.date_range('2020-01-01', '2020-04-05', freq='W')
+dates21=pd.date_range('2021-01-01', '2021-04-05', freq='W')
+dates=dates18.append(dates19).append(dates20).append(dates21)
+#print(pd.to_datetime(df.index))
+df.index=pd.to_datetime(df.index)
+#print(y_train)
+#plot tunnel for each week separately over years
+for d18,d19,d20,d21 in zip(list(dates18),list(dates19),list(dates20),list(dates21)):
+    plt.plot([2018,2019,2020,2021],df['Tunnel'].filter(items=[d18,d19,d20,d21],axis=0))
+#plt.plot([2018,2019,2020,2021],df.filter(items=[dates18[0],dates19[0],dates20[0],dates21[0]],axis=0))
 plt.show()
+#plot Steinlach for each week separately over years
+for d18,d19,d20,d21 in zip(list(dates18),list(dates19),list(dates20),list(dates21)):
+    plt.plot([2018,2019,2020,2021],df['Steinlach'].filter(items=[d18,d19,d20,d21],axis=0))
+plt.show()
+#plot Hirschau for each week separately over years
+for d18,d19,d20,d21 in zip(list(dates18),list(dates19),list(dates20),list(dates21)):
+    plt.plot([2018,2019,2020,2021],df['Hirschau'].filter(items=[d18,d19,d20,d21],axis=0))
+plt.show()
+
+#reg = LinReg().fit(X_train,y_train)
