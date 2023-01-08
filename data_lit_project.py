5e62e159058bcbb63e532a77ceff9f865d99998f
Fynn <fynnlu@gmail.com>
2023-01-08 14:42:15 +0100

    data filtering completed, new variable declariation for dates

--- data_lit_project.py
+++ data_lit_project.py
@@ -30,15 +30,45 @@ print(df.tail())
 #plt.plot(raw_df)
 #plt.plot(df)
 #plt.show()
+df.index=pd.to_datetime(df.index)
+
+############# Parameters ######################
+
+start_date_before="-01-01"
+end_date_before="-04-05"
+start_date_after="-01-01"
+end_date_after="-04-05"
+
 ############# Train data Part 1 #########################
 
-dates18=pd.date_range('2018-01-01', '2018-04-05', freq='W')
-dates19=pd.date_range('2019-01-01', '2019-04-05', freq='W')
-dates20=pd.date_range('2020-01-01', '2020-04-05', freq='W')
-dates21=pd.date_range('2021-01-01', '2021-04-05', freq='W')
+dates18=pd.date_range("2018"+start_date_before, "2018"+end_date_before, freq='W')
+dates19=pd.date_range("2019"+start_date_before, "2019"+end_date_before, freq='W')
+dates20=pd.date_range("2020"+start_date_before, "2020"+end_date_before, freq='W')
+dates21=pd.date_range("2021"+start_date_before, "2021"+end_date_before, freq='W')
 dates=dates18.append(dates19).append(dates20).append(dates21)
 #print(pd.to_datetime(df.index))
-df.index=pd.to_datetime(df.index)
+#print(y_train)
+#plot tunnel for each week separately over years
+for d18,d19,d20,d21 in zip(list(dates18),list(dates19),list(dates20),list(dates21)):
+    plt.plot([2018,2019,2020,2021],df['Tunnel'].filter(items=[d18,d19,d20,d21],axis=0))
+#plt.plot([2018,2019,2020,2021],df.filter(items=[dates18[0],dates19[0],dates20[0],dates21[0]],axis=0))
+plt.show()
+#plot Steinlach for each week separately over years
+for d18,d19,d20,d21 in zip(list(dates18),list(dates19),list(dates20),list(dates21)):
+    plt.plot([2018,2019,2020,2021],df['Steinlach'].filter(items=[d18,d19,d20,d21],axis=0))
+plt.show()
+#plot Hirschau for each week separately over years
+for d18,d19,d20,d21 in zip(list(dates18),list(dates19),list(dates20),list(dates21)):
+    plt.plot([2018,2019,2020,2021],df['Hirschau'].filter(items=[d18,d19,d20,d21],axis=0))
+plt.show()
+############# Train data Part 2 #########################
+
+dates18=pd.date_range("2018"+start_date_after, "2018"+end_date_after, freq='W')
+dates19=pd.date_range("2019"+start_date_after, "2019"+end_date_after, freq='W')
+dates20=pd.date_range("2020"+start_date_after, "2020"+end_date_after, freq='W')
+dates21=pd.date_range("2021"+start_date_after, "2021"+end_date_after, freq='W')
+dates=dates18.append(dates19).append(dates20).append(dates21)
+#print(pd.to_datetime(df.index))
 #print(y_train)
 #plot tunnel for each week separately over years
 for d18,d19,d20,d21 in zip(list(dates18),list(dates19),list(dates20),list(dates21)):
@@ -54,4 +84,12 @@ for d18,d19,d20,d21 in zip(list(dates18),list(dates19),list(dates20),list(dates2
     plt.plot([2018,2019,2020,2021],df['Hirschau'].filter(items=[d18,d19,d20,d21],axis=0))
 plt.show()
 
-#reg = LinReg().fit(X_train,y_train)
+############# Test data  #########################
+
+dates22_before = pd.date_range("2022"+start_date_before, "2022"+end_date_before, freq='W').date
+dates22_after = pd.date_range("2022"+start_date_after, "2022"+end_date_after, freq='W').date
+#print(pd.to_datetime(df.index))
+#print(y_train)
+#plot tunnel for each week separately over years
+cyclists22_before= df.filter(items=list(dates22_before),axis=0)
+cyclists22_after= df.filter(items=list(dates22_after),axis=0)
